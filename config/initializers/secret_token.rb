99af0fbea870c13abd05fc5e02ad3ba7a0307a66
conoromalley <conoromalley@gmail.com>
2013-09-17 13:11:49 -0400

    A blank dev project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AptanaCreated::Application.config.secret_key_base = '35e75fdc86e40b2067f8c357eda90568b6cf0f3c1aa98f7876b93493bff2dd6a51f4f8d79ed3d4dfdeb4c5e1eeb69b20358c5c6359d2c6a6bae911eb2613b9df'
